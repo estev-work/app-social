637de7d5bc14751b41555e7ec730d10485a19753
Alexei Gybchenko <alexey.g@pyrobyte.ru>
2024-03-11 01:29:25 +0700

    Рефакторинг PostId

--- src/Project/Post/Domain/ValueObjects/PostId.php
+++ src/Project/Post/Domain/ValueObjects/PostId.php
@@ -5,19 +5,16 @@ namespace App\Project\Post\Domain\ValueObjects;
 use Symfony\Component\Uid\Uuid;
 use Symfony\Component\Validator\Constraints as Assert;
 
-final class PostId
+final readonly class PostId
 {
-    #[Assert\Uuid]
+    #[Assert\Uuid(versions: 4)]
     private string $value;
 
     public function __construct(string $value = null)
     {
-        $this->value = $value ?? Uuid::v4();
+        $this->value = ($value == null) && empty($value) ? Uuid::v4()->toRfc4122() : $value;
     }
 
-    /**
-     * @return string|null
-     */
     public function getValue(): ?string
     {
         return $this->value;
